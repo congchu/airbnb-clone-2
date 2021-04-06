b9a7f935e8b15a67068fb141a9b1d08c99235cf7
congchu <cookie00421@gmail.com>
2021-04-06 16:32:17 +0900

    Room 모델 마이그레이션

--- core/models.py
+++ core/models.py
@@ -4,8 +4,8 @@ class TimeStampledModel(models.Model):
     
     """ TimeStampledModel """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
