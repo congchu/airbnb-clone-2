c60aa33c596f3e5735219d91102ae11f37420753
congchu <cookie00421@gmail.com>
2021-04-10 20:55:00 +0900

    UserAdmin: list_display와 list_filter 추가

--- users/admin.py
+++ users/admin.py
@@ -2,22 +2,37 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from . import models
 
+
 @admin.register(models.User)
-class  CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin):
     """ Custom User Admin"""
+
     fieldsets = UserAdmin.fieldsets + (
         (
-            "Custom Profile", 
+            "Custom Profile",
             {
                 "fields": (
-                    "avatar", 
-                    "gender", 
+                    "avatar",
+                    "gender",
                     "bio",
                     "birthday",
                     "language",
                     "currency",
                     "superhost",
                 )
-            }
+            },
         ),
     )
+
+    list_display = (
+        "username",
+        "email",
+        "is_active",
+        "language",
+        "currency",
+        "superhost",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
