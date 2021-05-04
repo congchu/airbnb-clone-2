c6470d690d0b45bb9af6a2e8669a8b8a6c686756
congchu <cookie00421@gmail.com>
2021-05-04 11:26:21 +0900

    #9.2 seed_everything and seed_users

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6dd%08@c&i8%cnln_#f1+t^_i&i71my&l31si4nkz_2zeq%$i)'
+SECRET_KEY = "6dd%08@c&i8%cnln_#f1+t^_i&i71my&l31si4nkz_2zeq%$i)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -29,69 +29,70 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-DJANGO_APPS=[
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',]
-
-THIRD_PARTY_APPS =[
-    'django_dountries'
-    ]
-
-PROJECT_APPS=[
-    'core.apps.CoreConfig',
-    'users.apps.UsersConfig',
-    'rooms.apps.RoomsConfig',
-    'reviews.apps.ReviewsConfig',
-    'reservations.apps.ReservationsConfig',
-    'lists.apps.ListsConfig',
-    'conversations.apps.ConversationsConfig',
-    
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+THIRD_PARTY_APPS = [
+    "django_countries",
+    "django_seed",
+]
+
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
+    "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
+    "conversations.apps.ConversationsConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -101,16 +102,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -118,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Asia/Seoul"
 
@@ -132,6 +133,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = "users.User"
--- rooms/management/commands/seed_amenities.py
+++ rooms/management/commands/seed_amenities.py
@@ -1,54 +1,21 @@
 from django.core.management.base import BaseCommand
-from rooms.models import Amenity
+from rooms.models import Facility
 
 
 class Command(BaseCommand):
+
+    help = "This command creates facilities"
+
     def handle(self, *args, **options):
-        amenities = [
-            "Air conditioning",
-            "Alarm Clock",
-            "Balcony",
-            "Bathroom",
-            "Bathtub",
-            "Bed Linen",
-            "Boating",
-            "Cable TV",
-            "Carbon monoxide detectors",
-            "Chairs",
-            "Children Area",
-            "Coffee Maker in Room",
-            "Cooking hob",
-            "Cookware & Kitchen Utensils",
-            "Dishwasher",
-            "Double bed",
-            "En suite bathroom",
-            "Free Parking",
-            "Free Wireless Internet",
-            "Freezer",
-            "Fridge / Freezer",
-            "Golf",
-            "Hair Dryer",
-            "Heating",
-            "Hot tub",
-            "Indoor Pool",
-            "Ironing Board",
-            "Microwave",
-            "Outdoor Pool",
-            "Outdoor Tennis",
-            "Oven",
-            "Queen size bed",
-            "Restaurant",
-            "Shopping Mall",
-            "Shower",
-            "Smoke detectors",
-            "Sofa",
-            "Stereo",
-            "Swimming pool",
-            "Toilet",
-            "Towels",
-            "TV",
+        facilities = [
+            "Private entrance",
+            "Paid Parking on premiese",
+            "Paid parking off premeses",
+            "Elevator",
+            "Parking",
+            "Gym",
         ]
 
-        for a in amenities:
-            Amenity.objects.create(name=a)
-        self.stdout.write(self.style.SUCCESS("Amenities created!"))
+        for f in facilities:
+            Facility.objects.create(name=f)
+        self.stdout.write(self.style.SUCCESS("Facilities created!"))
